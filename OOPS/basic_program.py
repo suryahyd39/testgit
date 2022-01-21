c35710b243fe82719d67aec2496f8f8532bff987
LAPTOP-N8IDHUUU\SURYA <udemy2021courses@gmail.com>
2022-01-21 23:08:55 +0530

    i reverted

--- OOPS/basic_program.py
+++ OOPS/basic_program.py
@@ -1,6 +1,10 @@
 print('Hello')
+<<<<<<< HEAD
 print("Hi")
 
 print('some matter after second line correction')
 
 print ('line 4')
+=======
+prin("Hi")
+>>>>>>> parent of 9b73c15... corrected second line
