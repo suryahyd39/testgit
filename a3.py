adeaa1849d4f14a41b47c064f008e2f3613e3782
LAPTOP-N8IDHUUU\SURYA <udemy2021courses@gmail.com>
2022-01-22 21:13:54 +0530

    reverted to secoond commit

--- a3.py
+++ a3.py
@@ -1,5 +1,9 @@
 print('first line added')
+<<<<<<< HEAD
 print('second line added')
 print('third line added')
 print('fourth line added')
 print('fifth line added')
+=======
+print('second line added')
+>>>>>>> parent of fd928ad... commit after third line
